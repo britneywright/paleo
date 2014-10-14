957b634f4fcebbee10feed4977ba3014578dcea5
Britney Wright <blw06g@gmail.com>
2014-10-14 15:33:05 -0400

    food controller and view updates

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -12,10 +12,40 @@ class FoodsController < ApplicationController
   def edit
   end
 
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to @food, notice: 'Food was successfully created.' }
+        format.json { render :show, status: :created, location: @food }
+      else
+        format.html { render :new }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end    
+  end
+
   def update
+    respond_to do |format|
+      if @food.update(food_params)
+        format.html { redirect_to @food, notice: 'Food was successfully updated.' }
+        format.json { render :show, status: :ok, location: @food }
+      else
+        format.html { render :edit }
+        format.json { render json: @food.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @food.destroy
+    format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+    format.json { head :no_content }
   end
 
   private
@@ -26,6 +56,6 @@ class FoodsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def food_params
-      params.require(:food).permit(:name)
+      params.require(:food).permit(:name, :paleo)
     end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,8 @@
 class Food < ActiveRecord::Base
-  validates :name, :paleo, presence: :true
+  validates :name, presence: :true
   validates :name, length: {minimum: 1,
     too_short: "must be at least 1 character"}
+  validates :paleo, inclusion: [true, false]  
 
   def self.search(query)
     where("name like ?", "%#{query}%")
