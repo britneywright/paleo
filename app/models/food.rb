efcf5afbfde171c2ced1f6d8f5311e818703c7f6
Britney Wright <blw06g@gmail.com>
2014-10-15 15:02:54 -0400

    add validations and param methods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,14 +1,18 @@
 class Food < ActiveRecord::Base
-  validates :name, presence: :true
+  validates :name, presence: true
   validates :name, length: {minimum: 1,
     too_short: "must be at least 1 character"}
-  validates :paleo, inclusion: [true, false]  
-
+  validates :name, uniqueness: true
+  validates :paleo, inclusion: [true, false] 
   before_validation :set_slug
 
-    def set_slug
-      self.slug = name.parameterize unless name.blank?
-    end
+  def set_slug
+    self.slug = name.parameterize unless name.blank?
+  end
+
+  def to_param
+    slug
+  end
 
   def self.search(query)
     where("name like ?", "%#{query}%")
